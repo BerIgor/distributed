ca97d0e44d3b04163759a7e62fc0e57e3641229f
IdanPeretz <idanpere1@gmail.com>
2018-05-08 13:15:30 +0300

    extracting column vector of matrix works

--- hw2/vectors.erl
+++ hw2/vectors.erl
@@ -1,12 +1,20 @@
-
 -module(vectors).
+-import(lists,[nth/2]).
+-import(lists,[append/2]).
 
 -export([multVectors/2]).
+-export([getColumnVec/2]).
 
 
 multVectors(TupleVec1,TupleVec2) when is_tuple(TupleVec1) == true , is_tuple(TupleVec1) == true , tuple_size(TupleVec1) == tuple_size(TupleVec2) ->
 	multVectors(tuple_to_list(TupleVec1),tuple_to_list(TupleVec2),0).
-multVectors(List1,List2,Sum) -> Sum+List1*List2;
-multVectors(Vec1,Vec2,Index,Sum) ->
+multVectors([Head1|List1], [Head2|List2], Sum) -> multVectors(List1, List2, Sum + Head1*Head2);
+multVectors([],[],Sum) -> Sum.
 
-.
+
+getColumnVec(Index,Matrix) when is_tuple(Matrix) == true ->
+	ListOfRowTuples = tuple_to_list(Matrix),
+	getColumnVec(Index,[],ListOfRowTuples).
+getColumnVec(Index, CurrentVec, [NextRowTuple|RowTuples]) ->
+	getColumnVec(Index, lists:append(CurrentVec, [element(Index,NextRowTuple)]), RowTuples);
+getColumnVec(_,CurrentVec,[]) -> CurrentVec.
