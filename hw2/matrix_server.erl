bd300f72f7817187d55adf60082e5be722d2c729
BerIgor <bigor11@gmail.com>
2018-05-09 15:11:13 +0300

    Fixed server. Mult now works

--- hw2/matrix_server.erl
+++ hw2/matrix_server.erl
@@ -4,7 +4,7 @@
 -export([mult/2]).
 %-export([get_version/0]).
 %-export([explanation/0]).
-
+%-export([assemble_matrix_loop/3]).
 
 start_server() ->
 	restarter().
@@ -15,7 +15,9 @@ mult(Mat1, Mat2) ->
 	ResMat_RowCount = tuple_size(Mat1),
 	ResMat_ColCount = tuple_size(element(1, Mat2)),
 	ZeroMat = matrix:getZeroMat(ResMat_RowCount, ResMat_ColCount),
-	Assembler_pid = spawn(?MODULE, assemble_matrix_loop, [ZeroMat, ResMat_RowCount*ResMat_ColCount, self()]),
+%	Assembler_pid = spawn(?MODULE, assemble_matrix_loop, [ZeroMat, ResMat_RowCount*ResMat_ColCount, self()]),
+	Self = self(),
+	Assembler_pid = spawn(fun()-> assemble_matrix_loop(ZeroMat, ResMat_RowCount*ResMat_ColCount, Self) end),
 	% Create processes to calculate multiplications
 	_ = [spawn(fun()-> vectors:getMultMatrixElement(Assembler_pid, RowNum, ColNum, Mat1, Mat2) end) || RowNum <- lists:seq(1,ResMat_RowCount), ColNum <- lists:seq(1,ResMat_ColCount)],
 	receive
@@ -28,11 +30,11 @@ mult(Mat1, Mat2) ->
 
 %%%%%%%%%%%%%%%%%%%%%%%% Private functions
 
-assemble_matrix_loop(Mat, 0, Parent_pid) ->
+assemble_matrix_loop(Mat, Element_count, Parent_pid) when Element_count == 0 ->
 	io:format("hello from assemble_matrix_loop with 0~n"),
 	Parent_pid ! {self(), Mat};
 assemble_matrix_loop(Mat, Element_count, Parent_pid) ->
-	io:format("hello from assemble_matrix_loop~n"),
+	io:format("hello from assemble_matrix_loop with~p~n", [Element_count]),
 	receive
 		{RowNum, ColNum, Value} ->
 			NewMat = matrix:setElementMat(RowNum,ColNum,Mat, Value),
